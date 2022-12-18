53f9fa8720aa3c7c4d359a0db9d6dd7df3cbcf86
Alex Barros <alexsilva.pilar@gmail.com>
2022-12-18 13:43:59 +1000

    course started

---
+++ forin/lib/pages/product_overview_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:forin/data/dummy_data.dart';
+import 'package:forin/models/Product.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  final List<Product> loadedProducts = dummyProducts;
+  ProductsOverviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Forin"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          itemCount: loadedProducts.length,
+          itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10),
+        ),
+      ),
+    );
+  }
+}
