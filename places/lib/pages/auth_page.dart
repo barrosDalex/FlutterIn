e00fa6864a214d79f4758d85aa91321865fc1a8e
Alex Barros <alex.silva123@fatec.sp.gov.br>
2023-11-01 17:06:05 -0300

    created places app

---
+++ places/lib/pages/auth_page.dart
@@ -0,0 +1,48 @@
+import 'package:places/components/auth_form.dart';
+import 'package:places/models/auth_form_data.dart';
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool _isLoading = false;
+
+  void _handleSubmit(AuthFormData formData) {
+    setState(() => _isLoading = true);
+
+    print('AuthPage...');
+    print(formData.email);
+
+    setState(() => _isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: AuthForm(onSubmit: _handleSubmit),
+            ),
+          ),
+          if (_isLoading)
+            Container(
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(0, 0, 0, 0.5),
+              ),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
